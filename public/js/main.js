4a3c6d4c138592c044640d5111cf3a1b0cafedab
Olivier-Daire <daire.olivier@gmail.com>
2016-03-28 12:34:28 +0200

    Test blur edito

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
-new WOW().init();
-
-var el = document.querySelector('#map');
-var test;
-$.getJSON('assets/world.json', function(data){
-	test = data;
-}).done(function(){
-		var map = new Smallworld(el, {
-			geojson: test,
-			center: [40, 10],
-			waterColor: '#ffffff',
-			landColor: '#dedede',
-			marker: [37.757719928168605, -122.43760000000003],
-			markerSize: 6,
-			markerColor: '#3399ff'
-		});
-	}
-);
-$(window).load(function() {
-	$('.news-header').addClass('active');
+$(window).on("scroll", function () {
+	$('.navbar').toggleClass('tiny', $(document).scrollTop() > 0);
 });
---
+++ public/js/news.js
@@ -0,0 +1,21 @@
+new WOW().init();
+
+var el = document.querySelector('#map');
+var mapData;
+$.getJSON('assets/world.json', function(data){
+	mapData = data;
+}).done(function(){
+		var map = new Smallworld(el, {
+			geojson: mapData,
+			center: [40, 10],
+			waterColor: '#ffffff',
+			landColor: '#dedede',
+			marker: [37.757719928168605, -122.43760000000003],
+			markerSize: 6,
+			markerColor: '#3399ff'
+		});
+	}
+);
+$(window).load(function() {
+	$('.news-header').addClass('active');
+});
