45d8216394c4c205c5dc29af18b1550a0e908bf8
Olivier-Daire <daire.olivier@gmail.com>
2016-03-30 19:25:00 +0200

    Header with search bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 $(window).on("scroll", function () {
 	$('.navbar').toggleClass('tiny', $(document).scrollTop() > 0);
 });
+
+$('header .btn-link').on('click', function(event) {
+	event.preventDefault();
+	$('header form').fadeToggle();
+});
